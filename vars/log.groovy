7212b20ff660f63681e8b83897befcb430cd48fc
eadesany <elijah.adesanya@oulu.fi>
2022-12-30 17:38:55 +0000

    ansible commits

---
+++ vars/log.groovy
@@ -0,0 +1,27 @@
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Code Quality') {
+                steps {
+                    echo 'Code Quality'
+                }
+            }
+            stage('Test Cases') {
+                steps {
+                    echo 'Test Cases'
+                }
+            }
+
+            stage('Publish A Release') {
+                steps {
+                    echo 'Publish A Release'
+                }
+            }
+        }
+    }
+
+}
+
