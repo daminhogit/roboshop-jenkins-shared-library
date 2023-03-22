bca3dc8097148ab382aca86cf34e45db02f00cec
eadesany <elijah.adesanya@oulu.fi>
2023-03-22 21:46:48 +0000

    Jenkins-Terraform commit

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -3,30 +3,27 @@ def call() {
 
         agent any
 
-
         parameters {
-            choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick Environment')
-            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick Terraform Action')
+            choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick Environment', defaultValue: '')
+            choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick Terraform Action', defaultValue: '')
         }
 
         stages {
-
             stage('Terraform Init') {
                 steps {
-                    sh '''
+                    sh """
                       terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
-                    '''
+                    """
                 }
             }
 
             stage('Terraform Plan') {
                 steps {
-                    sh '''
+                    sh """
                       terraform plan -var-file=env/${ENVIRONMENT}.tfvars
-                    '''
+                    """
                 }
             }
         }
-
     }
 }
